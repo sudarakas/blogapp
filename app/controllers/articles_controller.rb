b35977dffffd1c454e7f3c1fc7c1a198ce54aaf6
sudarakas <sudharakafb@gmail.com>
2019-01-26 15:33:11 +0530

    update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -28,7 +28,8 @@ class ArticlesController < ApplicationController
 
     respond_to do |format|
       if @article.save
-        format.html { redirect_to @article, notice: 'Your Article was successfully created.' }
+        flash[:success] = 'Your Article was successfully created.'
+        format.html { redirect_to @article}
         format.json { render :show, status: :created, location: @article }
       else
         format.html { render :new }
@@ -42,7 +43,8 @@ class ArticlesController < ApplicationController
   def update
     respond_to do |format|
       if @article.update(article_params)
-        format.html { redirect_to @article, notice: 'Your Article was successfully updated.' }
+        flash[:info] = 'Your Article was successfully updated.'
+        format.html { redirect_to @article}
         format.json { render :show, status: :ok, location: @article }
       else
         format.html { render :edit }
@@ -56,7 +58,8 @@ class ArticlesController < ApplicationController
   def destroy
     @article.destroy
     respond_to do |format|
-      format.html { redirect_to articles_url, notice: 'Your Article was successfully destroyed.' }
+      flash[:warning] = 'Your Article was successfully destroyed'
+      format.html { redirect_to articles_url}
       format.json { head :no_content }
     end
   end
